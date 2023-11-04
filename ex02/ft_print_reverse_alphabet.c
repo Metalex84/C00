9c554666891b31daf412fffb0a0bdb877ec51a8f
Alejandro Gonzalez Venegas <alex@MacBook-Air-de-Alejandro.local>
2023-11-04 13:15:30 +0100

    ft_print_reverse_alphabet with main

---
+++ ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alejango <alejango@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/10 18:04:10 by alejango          #+#    #+#             */
+/*   Updated: 2023/08/10 18:08:33 by alejango         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	my_character;
+
+	my_character = 'z';
+	while (my_character >= 'a')
+		ft_putchar(my_character--);
+}
