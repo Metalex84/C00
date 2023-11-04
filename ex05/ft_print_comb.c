1f281a7919341c503b40ef429c3a5a58e29a6035
Alejandro Gonzalez Venegas <alex@MacBook-Air-de-Alejandro.local>
2023-11-04 13:36:03 +0100

    ft_print_comb with main

---
+++ ex05/ft_print_comb.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alejango <alejango@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/10 20:50:53 by alejango          #+#    #+#             */
+/*   Updated: 2023/08/11 08:55:14 by alejango         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_comb(void)
+{
+	int	a;
+	int	b;
+	int	c;
+
+	a = '0';
+	while (a < '8')
+	{
+		b = a + 1;
+		while (b < '9')
+		{
+			c = b + 1;
+			while (c <= '9')
+			{
+				ft_putchar(a);
+				ft_putchar(b);
+				ft_putchar(c);
+				c++;
+				if (a != '7')
+					write(1, ", ", 2);
+			}
+			b++;
+		}
+		a++;
+	}
+}
