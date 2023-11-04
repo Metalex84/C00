fba1237e0c0e113500a8e99b97f3a15e424f2f8a
Alejandro Gonzalez Venegas <alex@MacBook-Air-de-Alejandro.local>
2023-11-04 13:19:31 +0100

    ft_print_numbers with main

---
+++ ex03/ft_print_numbers.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_numbers.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alejango <alejango@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/10 18:27:27 by alejango          #+#    #+#             */
+/*   Updated: 2023/08/10 18:38:19 by alejango         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_numbers(void)
+{
+	char	my_character;
+
+	my_character = '0';
+	while (my_character <= '9')
+		ft_putchar(my_character++);
+}
