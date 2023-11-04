3ad806330a328d0af17114912cd2dc7ba6d99747
Alejandro Gonzalez Venegas <alex@MacBook-Air-de-Alejandro.local>
2023-11-04 13:22:38 +0100

    ft_is_negative with main

---
+++ ex04/ft_is_negative.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_is_negative.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alejango <alejango@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/10 19:32:27 by alejango          #+#    #+#             */
+/*   Updated: 2023/08/10 21:17:10 by alejango         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_is_negative(int n)
+{
+	if (n < 0)
+		ft_putchar('N');
+	else
+		ft_putchar('P');
+}
---
+++ ex04/main.c
@@ -0,0 +1,11 @@
+void	ft_is_negative(int n);
+
+/* Expected output: NPP*/
+
+int	main(void)
+{
+	ft_is_negative(-2);
+	ft_is_negative(0);
+	ft_is_negative(4);
+	return (0);
+}
